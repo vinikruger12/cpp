68df571da2d3b3d9b6b8da4b31c0ae803dbc7480
Vinicius Krüger <krugerdv12@gmail.com>
2025-03-18 22:15:37 -0300

    Create parimp.cpp

---
+++ aula01brute/parimp.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a;
+    if(a%2 == 0){
+        cout << "O valor eh par" << endl;
+    }else{
+        cout << "O valor eh impar" << endl;
+    }
+}
