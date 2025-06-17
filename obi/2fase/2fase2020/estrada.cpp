219a5c77a8b938551a91050ac6037d978da94fc4
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2025-06-17 13:38:44 -0300

    obi

--- obi/2fase/2fase2020/estrada.cpp
+++ obi/2fase/2fase2020/estrada.cpp
@@ -12,17 +12,23 @@ int main(){
     }
 
     sort(vec.begin(), vec.end());
+
     vector<double> novo;
+    novo.emplace_back(0);
     double a = 0;
     for(int i = 1;i < n;i++){
          a += ((vec[i] + vec[i-1]) / 2);
          novo.emplace_back(((vec[i] + vec[i-1]) / 2));
     }
-  
-    if((t - a) > 0){
-        novo.emplace_back(t-a);
-    }
-    sort(novo.begin(), novo.end());
+    novo.emplace_back(t);
 
-    cout << fixed << setprecision(2) << novo[0] << endl;
+
+    double menor = t;
+    for(int i = 1;i < novo.size();i++){
+         menor = min((novo[i] - novo[i-1]), menor);
+    }
+
+   
+   
+    cout << fixed << setprecision(2) << menor << endl;
 }
