ff84a2e3b41dd1bf030e3f3fca1e7e52065cbe5f
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2025-07-21 22:04:11 -0300

    frog 2

---
+++ atcoder/dp/frog2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxn = 1e5+5;
+int dp[maxn], h[maxn];
+
+
+int main(){
+    int n,k; cin >> n >> k;
+    for(int i = 0;i < n;i++) cin >> h[i];
+    for(int i = 0;i < n;i++) dp[i] = 1e9;
+
+    dp[0] = 0;
+
+    for(int i = 0;i < n;i++){
+        
+        for(int j = 1;j <= k;j++){
+            dp[j] = min(dp[j],dp[i] + abs(h[j] - h[i]));
+        }
+
+    }
+
+    for(int i = 0;i< n;i++){
+        cout << dp[i] << ' ';
+    }
+
+
+}
