a98c9e3bf90cdc0436768329ff8314935f18f449
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2026-01-15 18:50:12 -0300

    b

--- contestCODEFORCE/div3/div3-1071/b.cpp
+++ contestCODEFORCE/div3/div3-1071/b.cpp
@@ -8,6 +8,17 @@ int main(){
 
     int t; cin >> t;
     while(t--){
-        
+        int n;cin >> n;
+        vector<int> a(n);
+        for(int i = 0;i < n;i++){
+            cin >> a[i];
+        }
+        int soma = 0;
+        for(int i = 0;i < n;i++){
+            if(i % 2 == 0) soma += a[i];
+            else soma -= a[i];
+        }
+
+        cout << soma << endl;
     }
 }
