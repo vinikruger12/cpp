fa80e806a2c4e64498c2941a64bbfe5e78cc7a3b
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2026-01-26 19:06:12 -0300

    code

--- contestCODEFORCE/coding.cpp
+++ contestCODEFORCE/coding.cpp
@@ -1,60 +1,23 @@
 #include <bits/stdc++.h>
-#define int long long
+
 
 using namespace std;
 
-const int maxn = 1e5+5;
-vector<int> adj[maxn];
-const int INF = 1e9;
-
-
-signed main(){
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, m, k; cin >> n >> m >> k;
+    int n, m; cin >> n >> m;
     vector<int> a(n);
     for(int i = 0;i < n;i++) cin >> a[i];
-    
-    for(int i = 0;i < m;i++){
-        int u,v; cin >> u >> v;
-        --u;
-        --v;
-        adj[u].emplace_back(v);
-        adj[v].emplace_back(u);
-    }
 
-    vector<unordered_set<int>> vis(n);
-
-    queue<pair<int,int>> q;
-    q.push({0, 0});
-    vis[0].insert(0);
-
-    int dist = 0;
-
-    while(!q.empty()){
-        int sz = q.size();
-        while(sz--){
-            auto [u, t] = q.front();
-            q.pop();
-
-            if(u == n-1){
-                cout << dist << endl;
-                return 0;
-            }
-
-            for(int v : adj[u]){
-                int nt = t + (a[u] != a[v]);
-                if(nt > k) continue;
-
-                if(!vis[v].count(nt)){
-                    vis[v].insert(nt);
-                    q.push({v, nt});
-                }
-            }
+    int j = -1;
+    for(int i = 0;i < n;i++){
+        if(a[i] >= m){
+            j = i+1;
+            break;
         }
-        dist++;
     }
+    cout << j << endl;
 
-    cout << -1 << endl;
 }
