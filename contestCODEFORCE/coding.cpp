5b0d700645697deb24d18399dd9b557a7bde5090
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2026-01-28 20:30:20 -0300

    Update coding.cpp

--- contestCODEFORCE/coding.cpp
+++ contestCODEFORCE/coding.cpp
@@ -1,26 +1,30 @@
 #include <bits/stdc++.h>
-#define int long long
 
 using namespace std;
 
-signed main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int x1, y1; cin >> x1 >> y1;
-    int x2, y2; cin >> x2 >> y2;
-
-    if(x1 > x2) cout << 0 << endl;
-    else if(y2 >= y1) cout << -1 << endl;
-    else{
-        int sum1 = x1, sum2 = x2;
-        int d = 0;
-        while(sum1 <= sum2){
-            sum1 += y1;
-            sum2 += y2;
-            d++;
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int x, y; cin >> x >> y;
+        
+        int res = 0;
+        while(x > 0 || y > 0){
+            if(y >= 2){
+                y -= 2;
+                x -= 7;
+                res++;
+            }
+            else if(y == 1){
+                y -= 1;
+                x -= 11;
+                res++;
+            }
+            else if(y == 0){
+                x -= 15;
+                res++;
+            }
         }
-        cout << d << endl;
+        cout << res << endl;
+        
     }
-
 }
