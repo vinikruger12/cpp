7571e876170ee5c5edc57768056b579aaa788f50
vinikruger12 <krugerdv12@gmail.com>
2026-02-02 23:21:46 -0300

    bs

--- contestCODEFORCE/coding.cpp
+++ contestCODEFORCE/coding.cpp
@@ -1,47 +1,39 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
+vector<int> a;
+int n;
 
-int main(){
+void bs(int tempo){
+    int l = -1, r = n;
+    int mid = (l+r)/2;
+
+    while(r - l > 1){
+
+        mid = (l+r)/2;
+        if(a[mid] > tempo){
+            r = mid;
+        }
+        else{
+            l = mid;
+        }
+    }
+
+    cout << l + 1 << endl;
+
+}
+
+signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t; 
-    cin >> t;
-    while(t--){
-        int n; 
-        cin >> n;
-        vector<int> a(n);
-        for(int i = 0; i < n; i++) cin >> a[i];
-        int i = 0;
+   int k; cin >> n >> k;
+   int tempo = 240 - k;
+   a.resize(n);
+   a[0] = 5;
+   for(int i = 1;i < n;i++){
+    a[i] = ((i + 1) * 5) + a[i-1];
+   } 
 
-        while(i < n && a[i] == n - i){
-            cout << a[i] << ' ';
-            i++;
-        }
-
-        if(i == n){
-            cout << endl;
-            continue;
-        }
-
-        int acha = n - i;
-        int r = -1;
-
-        for(int j = i; j < n; j++){
-            if(a[j] == acha){
-                r = j;
-                break;
-            }
-        }
-
-        for(int j = r; j >= i; j--){
-            cout << a[j] << ' ';
-        }
-
-        for(int j = r + 1; j < n; j++){
-            cout << a[j] << ' ';
-        }
-
-        cout << endl;
-    }
+   bs(tempo);
 }
