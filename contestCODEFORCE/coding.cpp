d1d637d87ac399544f199c30a1cc8930d3631598
vinikruger12 <krugerdv12@gmail.com>
2026-02-02 21:10:25 -0300

    a

--- contestCODEFORCE/coding.cpp
+++ contestCODEFORCE/coding.cpp
@@ -1,30 +1,47 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main(){
-    int t; cin >> t;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; 
+    cin >> t;
     while(t--){
-        int x, y; cin >> x >> y;
-        
-        int res = 0;
-        while(x > 0 || y > 0){
-            if(y >= 2){
-                y -= 2;
-                x -= 7;
-                res++;
-            }
-            else if(y == 1){
-                y -= 1;
-                x -= 11;
-                res++;
-            }
-            else if(y == 0){
-                x -= 15;
-                res++;
+        int n; 
+        cin >> n;
+        vector<int> a(n);
+        for(int i = 0; i < n; i++) cin >> a[i];
+        int i = 0;
+
+        while(i < n && a[i] == n - i){
+            cout << a[i] << ' ';
+            i++;
+        }
+
+        if(i == n){
+            cout << endl;
+            continue;
+        }
+
+        int acha = n - i;
+        int r = -1;
+
+        for(int j = i; j < n; j++){
+            if(a[j] == acha){
+                r = j;
+                break;
             }
         }
-        cout << res << endl;
-        
+
+        for(int j = r; j >= i; j--){
+            cout << a[j] << ' ';
+        }
+
+        for(int j = r + 1; j < n; j++){
+            cout << a[j] << ' ';
+        }
+
+        cout << endl;
     }
 }
