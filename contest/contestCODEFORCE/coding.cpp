db91109a22aa3fbb40f617daaae396ab7c707329
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2026-02-05 17:50:56 -0300

    some questions

--- contest/contestCODEFORCE/coding.cpp
+++ contest/contestCODEFORCE/coding.cpp
@@ -2,46 +2,29 @@
 
 using namespace std;
 
-vector<int> a;
-int l, n;
-
-int f(double mid){
-    double atual = 0;
-    
-    for(int i = 0;i < n;i++){
-        if(a[i] - mid > atual) return 0;
-        atual = max(atual, a[i] + mid);
-    }
-
-
-    if(atual >= l) return 1;
-
-    return 0;
-
-}
-
-
-signed main(){
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> n >> l;
-    a.resize(n);
+   int t; cin >> t;
+   while(t--){
+    int n; cin >> n;
+    vector<int> a(n);
+
     for(int i = 0;i < n;i++) cin >> a[i];
 
-    sort(a.begin(), a.end());
+    bool ok = 0;
 
-    double lo = 0, ro = (double)l;
-    
-    for(int i = 0;i < 100;i++){
-        double mid = (ro+lo)/2;
-        if(f(mid)){
-            ro = mid;
-        }
-        else{
-            lo = mid;
-        }
+    for(int i = 1;i < n;i++){
+        if(a[0] == '0' && a[i-1] == 0) ok = 1;
+        if(a[i] == '1' && a[i-1] == '0' && a[i+1] == '0' && n == 3) ok = 1;
+
     }
-    
-    cout << setprecision(10) << fixed << ro << endl;
+
+
+    if(ok) cout << "BOB" << endl;
+    else cout << "ALICE" << endl;
+
+   }
+
 }
