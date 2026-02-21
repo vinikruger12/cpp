da6d60f1769e267acdfd41293c1a4d7eff8bae6d
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2026-02-21 19:26:50 -0300

    cf

--- contest/contestCODEFORCE/coding.cpp
+++ contest/contestCODEFORCE/coding.cpp
@@ -3,25 +3,9 @@
 using namespace std;
 
 int main(){
-    int n; cin >> n;
-    vector<int> a(n);
-    for(int i = 0;i < n;i++) cin >> a[i];
-    int t; cin >> t;
-    
-    map<int, pair<int, int>> hash;
+   int n, m; cin >> n >> m;
 
-    for(int i = 0;i < n;i++){
-       
-        if(hash[a[i]].first >= 1){
-            cout << hash[a[i]].second << ' ' << i << endl;
-            break;
-        }
-
-        int k = t - a[i];
-        hash[k].first++;
-        hash[k].second = i;
-    }
-    
+   cout << (n*m) / 2 << endl;
 
 
 }
