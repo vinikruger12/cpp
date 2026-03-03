c7e8dc14a8f014e8048d9c9040c718f8808ead41
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2026-03-02 21:07:12 -0300

    cf

--- contest/contestCODEFORCE/coding.cpp
+++ contest/contestCODEFORCE/coding.cpp
@@ -4,13 +4,6 @@ using namespace std;
 
 
 signed main(){
-    int n, k; cin >> n >> k;
-    int q = n % 2 == 0 ? (int)n/2 : (int)n/2 + 1;
-    if(k <= q){
-       cout << 2*k - 1 << endl; 
-    }
-    else{
-       cout << 2*(k - q) << endl;
-    }
-
+   int n; cin >> n;
+   cout << 25 << endl;
 }   
