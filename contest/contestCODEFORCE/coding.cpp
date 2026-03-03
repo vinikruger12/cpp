1c1ab6f4c15f51f1deea069aeba1bda6f7729c61
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2026-03-03 10:50:03 -0300

    cf

--- contest/contestCODEFORCE/coding.cpp
+++ contest/contestCODEFORCE/coding.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 
 signed main(){
-   int n; cin >> n;
-   cout << 25 << endl;
+   int mat[5][5];
+   int m = 0, n = 0;
+   for(int i = 0;i < 5;i++){
+      for(int j = 0;j < 5;j++){
+         int x; cin >> x;
+         if(x == 1){
+            m = i;
+            n = j;
+         }
+      }
+   }
+
+   cout << (abs(m - 2) + abs(n - 2)) << endl;
+
+
 }   
