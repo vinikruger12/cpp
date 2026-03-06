bbeb97864f8d434bdc7faa5f846990e4e1bbbb40
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2026-03-05 21:09:15 -0300

    cf

--- contest/contestCODEFORCE/coding.cpp
+++ contest/contestCODEFORCE/coding.cpp
@@ -2,21 +2,23 @@
 #define int long long
 using namespace std;
 
+bool f(int x){
+   string s = to_string(x);
+   sort(s.begin(), s.end());
+   bool ok = 1;
+   for(int i = 1;i < 4;i++){
+      if(s[i] == s[i-1]) ok = 0;
+   }
+   return ok;
+}
 
 signed main(){
-   int n, m, a; cin >> n >> m >> a;
-   int b = 0;
-   if(n % a == 0) b = n/a;
-   else b = (n/a) + 1; 
-
-   int c = 0;
-   int k = m - a;
-   int j = 0;
-   if(k > 0){
-      if(k % a == 0) j = k/a;
-      else j = k/a + 1;
+   int x; cin >> x;
+   int k = 1;
+   while(1){
+      if(f(k + x)) break;
+      else k++; 
    }
 
-   cout << b + (b*j) << endl;
-
+   cout << x + k << endl;
 }   
