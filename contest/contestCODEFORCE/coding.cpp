3c14a2f952268f8f5acef2c8ed3d875a7fcc3cb8
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2026-03-08 17:40:31 -0300

    cf

--- contest/contestCODEFORCE/coding.cpp
+++ contest/contestCODEFORCE/coding.cpp
@@ -5,35 +5,13 @@ using namespace std;
 
 signed main(){
    string s; cin >> s;
-   bool ok = 0;
-   map<char, int> mapa;
-   char c = s[0];
-   int a = 0, b = 0;
+   int ans = 0;
    for(int i = 0;i < s.size();i++){
-      if(s[i] == '1') a = 1;
-      if(s[i] == '0') b = 1;
+      if(ans == 0 && s[i] == 'h') ans++;
+      if(ans == 1 && s[i] == 'e') ans++;
+      if((ans == 2 || ans == 3) && s[i] == 'l') ans++;
+      if(ans == 4 && s[i] == 'o') ans++;
    }
-
-   for(int i = 0;i < s.size();i++){
-
-      if(mapa[s[i]] >= 7){
-         if(s[i] == '1') if(b) ok = 1;
-         if(s[i] == '0') if(a) ok = 1;
-      }
-
-      if(s[i] != c){
-         if(mapa[c] >= 7) ok = 1;
-         mapa[c] = 0;
-      }
-      mapa[s[i]]++;
-      c = s[i];
-   }
-
-   if(mapa[c] >= 7){
-         if(c == '1') if(b) ok = 1;
-         if(c == '0') if(a) ok = 1;
-      }
-
-   if(ok) cout << "YES" << endl;
+   if(ans == 5) cout << "YES" << endl;
    else cout << "NO" << endl;
 }   
