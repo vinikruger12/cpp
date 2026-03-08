d47db49cefd1be7dd4bc71132c0cce020dad4d2b
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2026-03-08 09:55:37 -0300

    cf

--- contest/contestCODEFORCE/coding.cpp
+++ contest/contestCODEFORCE/coding.cpp
@@ -6,14 +6,33 @@ using namespace std;
 signed main(){
    string s; cin >> s;
    bool ok = 0;
-   int k = 1;
-   for(int i = 1;i < s.size();i++){
-      if(s[i] == s[i-1]) k++;
-      else{
-         if(k == 7) ok = 1;
-         k = 1;
-      }
+   map<char, int> mapa;
+   char c = s[0];
+   int a = 0, b = 0;
+   for(int i = 0;i < s.size();i++){
+      if(s[i] == '1') a = 1;
+      if(s[i] == '0') b = 1;
    }
+
+   for(int i = 0;i < s.size();i++){
+
+      if(mapa[s[i]] >= 7){
+         if(s[i] == '1') if(b) ok = 1;
+         if(s[i] == '0') if(a) ok = 1;
+      }
+
+      if(s[i] != c){
+         if(mapa[c] >= 7) ok = 1;
+         mapa[c] = 0;
+      }
+      mapa[s[i]]++;
+      c = s[i];
+   }
+
+   if(mapa[c] >= 7){
+         if(c == '1') if(b) ok = 1;
+         if(c == '0') if(a) ok = 1;
+      }
 
    if(ok) cout << "YES" << endl;
    else cout << "NO" << endl;
