d47f55bf1e2267a97725c74f0d253c9a279bc895
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2026-03-07 23:59:08 -0300

    cf

--- contest/contestCODEFORCE/coding.cpp
+++ contest/contestCODEFORCE/coding.cpp
@@ -5,9 +5,16 @@ using namespace std;
 
 signed main(){
    string s; cin >> s;
-   if(isupper(s[0])) cout << s << endl;
-   else{
-      char k = toupper(s[0]);
-      cout << k << s.substr(1, s.size()) << endl;
-   } 
+   bool ok = 0;
+   int k = 1;
+   for(int i = 1;i < s.size();i++){
+      if(s[i] == s[i-1]) k++;
+      else{
+         if(k == 7) ok = 1;
+         k = 1;
+      }
+   }
+
+   if(ok) cout << "YES" << endl;
+   else cout << "NO" << endl;
 }   
