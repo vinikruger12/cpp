f4f48866688a176bd75e3adac03e7b8c07d824ff
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2026-03-11 19:22:12 -0300

    cf

--- contest/contestCODEFORCE/coding.cpp
+++ contest/contestCODEFORCE/coding.cpp
@@ -4,31 +4,24 @@ using namespace std;
 
 
 signed main(){
-   int maior = 0, menor = 0;
    string s; cin >> s;
+   int many = 0;
    for(int i = 0;i < s.size();i++){
-      if(s[i] >= 'a' && s[i] <= 'z') menor++;
-      else maior++;
+      if(s[i] == '7' || s[i] == '4') many++;
    }
 
-   if(maior > menor){
-      for(int i = 0;i < s.size();i++){
-         if(s[i] >= 'A' && s[i] <= 'Z') cout << s[i];
-         else{
-            char c = toupper(s[i]);
-            cout << c;
-         }
+   string k = to_string(many);
+
+   bool ok = 1;
+   for(int i = 0;i < k.size();i++){
+      if(k[i] != '7' && k[i] != '4') {
+         ok = 0;
+         break;
       }
-      cout << endl;
    }
-   else{
-      for(int i = 0;i < s.size();i++){
-         if(s[i] >= 'a' && s[i] <= 'z') cout << s[i];
-         else{
-            char c = tolower(s[i]);
-            cout << c;
-         }
-      }
-      cout << endl;      
-   }
+
+   if(ok) cout << "YES" << endl;
+   else cout << "NO" << endl;
+   
+
 }   
